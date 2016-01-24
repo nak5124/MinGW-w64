5fe5c2150d89e92aa06dd94f13012bb46c40ccb3
Yuta NAKAI <nak5124@live.jp>
2016-01-24 12:27:57 +0900

    search.h: Merged search_s.h into search.h.
     + Add missing function declarations.
     + Formatting cosmetics.

--- mingw-w64-headers/crt/search.h
+++ mingw-w64-headers/crt/search.h
@@ -11,20 +11,27 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif  /* __cplusplus */
 
 #ifndef _CRT_ALGO_DEFINED
-#define _CRT_ALGO_DEFINED
-  void *__cdecl bsearch(const void *_Key,const void *_Base,size_t _NumOfElements,size_t _SizeOfElements,int (__cdecl *_PtFuncCompare)(const void *,const void *));
-  void __cdecl qsort(void *_Base,size_t _NumOfElements,size_t _SizeOfElements,int (__cdecl *_PtFuncCompare)(const void *,const void *));
-#endif
-  _CRTIMP void *__cdecl _lfind(const void *_Key,const void *_Base,unsigned int *_NumOfElements,unsigned int _SizeOfElements,int (__cdecl *_PtFuncCompare)(const void *,const void *));
-  _CRTIMP void *__cdecl _lsearch(const void *_Key,void *_Base,unsigned int *_NumOfElements,unsigned int _SizeOfElements,int (__cdecl *_PtFuncCompare)(const void *,const void *));
+  _CRTIMP       void * __cdecl bsearch_s(const void *_Key, const void *_Base, rsize_t _NumOfElements, rsize_t _SizeOfElements, int (__cdecl *_PtFuncCompare)(void *, const void *, const void *), void *_Context);
+  /* _CRTIMP */ void * __cdecl bsearch(const void *_Key, const void *_Base, size_t _NumOfElements, size_t _SizeOfElements, int (__cdecl *_PtFuncCompare)(const void *, const void *));
 
-#ifndef	NO_OLDNAMES
-  void *__cdecl lfind(const void *_Key,const void *_Base,unsigned int *_NumOfElements,unsigned int _SizeOfElements,int (__cdecl *_PtFuncCompare)(const void *,const void *)) __MINGW_ATTRIB_DEPRECATED_MSVC2005;
-  void *__cdecl lsearch(const void *_Key,void *_Base,unsigned int *_NumOfElements,unsigned int _SizeOfElements,int (__cdecl *_PtFuncCompare)(const void *,const void *)) __MINGW_ATTRIB_DEPRECATED_MSVC2005;
-#endif
+  _CRTIMP       void   __cdecl qsort_s(void *_Base, rsize_t _NumOfElements, rsize_t _SizeOfElements, int (__cdecl *_PtFuncCompare)(void *, const void *, const void *), void *_Context);
+  /* _CRTIMP */ void   __cdecl qsort(void *_Base, size_t _NumOfElements, size_t _SizeOfElements, int (__cdecl *_PtFuncCompare)(const void *, const void *));
+#define _CRT_ALGO_DEFINED
+#endif  /* !_CRT_ALGO_DEFINED */
+
+  _CRTIMP void * __cdecl _lfind_s(const void *_Key, const void *_Base, unsigned int *_NumOfElements, size_t _SizeOfElements, int (__cdecl *_PtFuncCompare)(void *, const void *, const void *), void *_Context);
+  _CRTIMP void * __cdecl _lfind(const void *_Key, const void *_Base, unsigned int *_NumOfElements, unsigned int _SizeOfElements, int (__cdecl *_PtFuncCompare)(const void *, const void *));
+
+  _CRTIMP void * __cdecl _lsearch_s(const void *_Key, void *_Base, unsigned int *_NumOfElements, size_t _SizeOfElements, int (__cdecl *_PtFuncCompare)(void *, const void *, const void *), void *_Context);
+  _CRTIMP void * __cdecl _lsearch(const void *_Key, void *_Base, unsigned int *_NumOfElements, unsigned int _SizeOfElements, int (__cdecl *_PtFuncCompare)(const void *, const void *));
+
+#ifndef NO_OLDNAMES
+  /* _CRTIMP */ void * __cdecl lfind(const void *_Key, const void *_Base, unsigned int *_NumOfElements, unsigned int _SizeOfElements, int (__cdecl *_PtFuncCompare)(const void *, const void *)) __MINGW_ATTRIB_DEPRECATED_MSVC2005;
+  /* _CRTIMP */ void * __cdecl lsearch(const void *_Key, void *_Base, unsigned int *_NumOfElements, unsigned int _SizeOfElements, int (__cdecl *_PtFuncCompare)(const void *, const void *)) __MINGW_ATTRIB_DEPRECATED_MSVC2005;
+#endif  /* !NO_OLDNAMES */
 
 /*
 Documentation for these POSIX definitions and prototypes can be found in 
@@ -33,39 +40,37 @@ IEEE Std 1003.1, 2004 Edition.
 eg:  http://www.opengroup.org/onlinepubs/009695399/functions/twalk.html
 */
 
-typedef struct entry {
-	char *key;
-	void *data;
-} ENTRY;
+  typedef struct entry {
+    char *key;
+    void *data;
+  } ENTRY;
 
-typedef enum {
-	FIND,
-	ENTER
-} ACTION;
+  typedef enum {
+    FIND,
+    ENTER
+  } ACTION;
 
-typedef enum {
-	preorder,
-	postorder,
-	endorder,
-	leaf
-} VISIT;
+  typedef enum {
+    preorder,
+    postorder,
+    endorder,
+    leaf
+  } VISIT;
 
 #ifdef _SEARCH_PRIVATE
-typedef struct node {
-	char         *key;
-	struct node  *llink, *rlink;
-} node_t;
-#endif
-
-void * __cdecl tdelete (const void * __restrict__, void ** __restrict__, int (*)(const void *, const void *)) __MINGW_ATTRIB_NONNULL (2) __MINGW_ATTRIB_NONNULL (3);
-void * __cdecl tfind (const void *, void * const *, int (*)(const void *, const void *)) __MINGW_ATTRIB_NONNULL (2) __MINGW_ATTRIB_NONNULL (3);
-void * __cdecl tsearch (const void *, void **, int (*)(const void *, const void *)) __MINGW_ATTRIB_NONNULL (2) __MINGW_ATTRIB_NONNULL (3);
-void __cdecl twalk (const void *, void (*)(const void *, VISIT, int));
+  typedef struct node {
+    char   *key;
+    struct  node *llink, *rlink;
+  } node_t;
+#endif  /* _SEARCH_PRIVATE */
+  /* Provided in libmingwex. */
+  void * __cdecl tdelete(const void * __restrict__ vkey, void ** __restrict__ vrootp, int (*compare)(const void *, const void *)) __MINGW_ATTRIB_NONNULL(2) __MINGW_ATTRIB_NONNULL(3);
+  void * __cdecl tfind(const void *vkey, void * const *vrootp, int (*compare)(const void *, const void *)) __MINGW_ATTRIB_NONNULL(2) __MINGW_ATTRIB_NONNULL(3);
+  void * __cdecl tsearch(const void * __restrict__ vkey, void ** __restrict__ vrootp, int (*compare)(const void *, const void *)) __MINGW_ATTRIB_NONNULL(2) __MINGW_ATTRIB_NONNULL(3);
+  void   __cdecl twalk(const void *vroot, void (*action)(const void *, VISIT, int));
 
 #ifdef __cplusplus
 }
-#endif
+#endif  /* __cplusplus */
 
-#include <sec_api/search_s.h>
-
-#endif
+#endif  /* _INC_SEARCH */
