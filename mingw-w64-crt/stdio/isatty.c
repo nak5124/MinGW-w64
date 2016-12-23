31c95b725c9f71794680492a2015f433a714a8bc
Yuta Nakai <nak5124@live.jp>
2016-12-24 07:00:13 +0900

    stdio/isatty: Cygwin -> MSYS2.

--- mingw-w64-crt/stdio/isatty.c
+++ mingw-w64-crt/stdio/isatty.c
@@ -6,21 +6,21 @@
 
 int __cdecl (* const __MINGW_IMP_SYMBOL(isatty))(int) = isatty;
 
-/* Cygwin-compatible isatty.
+/* MSYS2-compatible isatty.
  *
- * Cygwin pty is a specially-named named pipe.
+ * MSYS2 pty is a specially-named named pipe.
  * Fetch [absolute] fd's NT object path (if any),
  * and check it for the following pattern:
  *
- *   \Device\NamedPipe\cygwin-[a-fA-F0-9]{16}-pty[0-9]{1,4}-(from-master|to-master|to-master-cyg)
+ *   \Device\NamedPipe\msys-[a-fA-F0-9]{16}-pty[0-9]{1,4}-(from-master|to-master|to-master-cyg)
  *
- * [a-fA-F0-9] is the cygwin installation key, 16 characters long.
+ * [a-fA-F0-9] is the msys2 installation key, 16 characters long.
  * pty[0-9] is the pty name. Its index is of type int, but is safe to be limited to 4 characters.
  *
  * */
 
 int __cdecl isatty(int fd) {
-  wchar_t const expect_dev[] = L"\\Device\\NamedPipe\\cygwin-";
+  wchar_t const expect_dev[] = L"\\Device\\NamedPipe\\msys-";
   wchar_t const expect_pty[] = L"-pty";
 
   typedef NTSTATUS (NTAPI proc_NtQueryObject) (HANDLE, OBJECT_INFORMATION_CLASS, PVOID, ULONG, PULONG);
@@ -39,33 +39,26 @@ int __cdecl isatty(int fd) {
   int l;
 
   h_fd = (HANDLE) _get_osfhandle(fd);
-  if (!h_fd || h_fd == INVALID_HANDLE_VALUE) {
-    errno = EBADF;
-    return 0;
-  }
+  if (!h_fd || h_fd == INVALID_HANDLE_VALUE)
+    goto bad_fp;
 
-  h_ntdll = GetModuleHandle("ntdll.dll");
-  if (!h_ntdll || h_ntdll == INVALID_HANDLE_VALUE) {
-    errno = EBADF;
-    return 0;
-  }
+  h_ntdll = GetModuleHandleW(L"ntdll.dll");
+  if (!h_ntdll || h_ntdll == INVALID_HANDLE_VALUE)
+    goto bad_fp;
 
   pNtQueryObject = (proc_NtQueryObject*) GetProcAddress(h_ntdll, "NtQueryObject");
-  if (!pNtQueryObject) {
+  if (!pNtQueryObject)
     goto no_tty;
-  }
 
   memset(ntfn, 0, ntfn_size);
-  status = pNtQueryObject((HANDLE)h_fd, ObjectNameInformation,
-      ntfn, ntfn_size, &ntfn_size);
+  status = pNtQueryObject((HANDLE)h_fd, ObjectNameInformation, ntfn, ntfn_size, &ntfn_size);
 
   if (!NT_SUCCESS(status)) {
     /* If it is not NUL (i.e. \Device\Null, which would succeed),
      * then normal isatty() could be consulted.
      * */
-    if (_isatty(fd)) {
+    if (_isatty(fd))
       return 1;
-    }
     goto no_tty;
   }
 
@@ -73,37 +66,35 @@ int __cdecl isatty(int fd) {
   s[ntfn->Name.Length / sizeof(WCHAR)] = 0;
 
   l = wcslen(expect_dev);
-  if (wcsncmp(s, expect_dev, l) != 0) {
+  if (wcsncmp(s, expect_dev, l) != 0)
     goto no_tty;
-  }
   s += l;
 
   l = wcsspn(s, L"0123456789abcdefABCDEF");
-  if (l != 16) {
+  if (l != 16)
     goto no_tty;
-  }
   s += l;
 
   l = wcslen(expect_pty);
-  if (wcsncmp(s, expect_pty, l) != 0) {
+  if (wcsncmp(s, expect_pty, l) != 0)
     goto no_tty;
-  }
   s += l;
 
   l = wcsspn(s, L"0123456789");
-  if (l < 1 || l > 4) {
+  if (l < 1 || l > 4)
     goto no_tty;
-  }
   s += l;
 
   if (wcscmp(s, L"-from-master") != 0 &&
         wcscmp(s, L"-to-master") != 0 &&
         wcscmp(s, L"-to-master-cyg") != 0)
-  {
     goto no_tty;
-  }
 
   return 1;
+
+bad_fp:
+  errno = EBADF;
+  return 0;
 
 no_tty:
   errno = EINVAL;
